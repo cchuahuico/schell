b12050e92c7a585681a1862e809f4e21fb23e629
Clarence Chuahuico <cchuahuico@gmail.com>
2013-03-18 19:23:27 -0700

    Refactored to use exceptions for error handling since adding variables/assignments will require everything to be in an IO monad

--- Schell/Main.hs
+++ Schell/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.Exit
 import Control.Monad
+import Control.Monad.Error
 import System.IO
 import Schell
 
@@ -9,13 +10,15 @@ main :: IO ()
 main = forever $ do
     putStr "schell> " >> hFlush stdout
     input <- getLine
+    env <- createEnv
     if input == ":q" 
       then
           exitSuccess
        else
           case parseSource input of 
             (Left err) -> putStrLn $ show err
-            (Right expr) -> 
-                case eval expr of
-                    (Left err) -> putStrLn err
-                    (Right res) -> putStrLn . show $ res
+            (Right expr) -> do
+              res <- liftIO . runErrorT . eval env $ expr
+              case res of
+                (Left err) -> putStrLn . show $ err
+                (Right res) -> putStrLn . show $ res
--- Schell/Schell.hs
+++ Schell/Schell.hs
@@ -1,7 +1,8 @@
 module Schell (
-    Expr(Number, String, Symbol, Boolean, List),
-    parseSource,
-    eval
+  Expr(Number, String, Symbol, Boolean, List),
+  parseSource,
+  eval,
+  createEnv
 ) where
 
 import Text.ParserCombinators.Parsec
@@ -9,88 +10,105 @@ import Data.Char
 import Data.Either
 import Text.Printf
 import Control.Monad
+import Control.Monad.Error
+import Data.IORef
 
 data Expr = Number Integer
-          | String String 
-          | Symbol String
-          | Boolean Bool
-          | List [Expr]
+      | String String 
+      | Symbol String
+      | Boolean Bool
+      | List [Expr]
 
 instance Show Expr where
-    show (Number num) = show num
-    show (String str) = show str
-    show (Symbol sym) = sym
-    show (Boolean bool) = if bool then "#t" else "#f"
-    show (List exprs) = "(" ++ (joinOn " " $ map show exprs) ++ ")"
+  show (Number num) = show num
+  show (String str) = show str
+  show (Symbol sym) = sym
+  show (Boolean bool) = if bool then "#t" else "#f"
+  show (List exprs) = "(" ++ (joinOn " " $ map show exprs) ++ ")"
+
+instance Eq Expr where
+  Number n1 == Number n2 = n1 == n2
+  String s1 == String s2 = s1 == s2
+  Symbol s1 == Symbol s2 = s1 == s2
+  Boolean b1 == Boolean b2 = b1 == b2
+  List l1 == List l2 = l1 == l2
+  _ == _ = False
+
+data EvalError = BindError String
+         | SyntaxError String
+
+instance Error EvalError where
+  strMsg s = SyntaxError "error"
+
+instance Show EvalError where
+  show (BindError var) = printf "**Error: %s not bound.**" var
+  show (SyntaxError msg) = printf "**Error: %s.**" msg
+
+type Env = IORef [(String, IORef Expr)]
+
+createEnv :: IO Env
+createEnv = newIORef []
 
 joinOn :: String -> [String] -> String
 joinOn _ [x] = x
 joinOn joinStr (x:xs) = (x ++ joinStr) ++ (joinOn joinStr xs)
 
-instance Eq Expr where
-    Number n1 == Number n2 = n1 == n2
-    String s1 == String s2 = s1 == s2
-    Symbol s1 == Symbol s2 = s1 == s2
-    Boolean b1 == Boolean b2 = b1 == b2
-    List l1 == List l2 = l1 == l2
-    _ == _ = False
-
 identifierInit :: Parser Char
 identifierInit = oneOf "!$%&*/:<=>?~_^" 
 
 readExpr = do
-    readLst <|> readString <|> readNumber <|> readSymbol <|> readBoolean 
+  readLst <|> readString <|> readNumber <|> readSymbol <|> readBoolean 
 
 readSymbol = do
-    initial <- letter <|> identifierInit <|> oneOf "+-"
-    subsq <- many (letter <|> identifierInit <|> digit <|> oneOf ".+-")
-    return $ Symbol (initial:subsq)
+  initial <- letter <|> identifierInit <|> oneOf "+-"
+  subsq <- many (letter <|> identifierInit <|> digit <|> oneOf ".+-")
+  return $ Symbol (initial:subsq)
 
 readLst = do
-    char '('
-    x <- sepBy readExpr spaces
-    char ')'
-    return $ List x
+  char '('
+  x <- sepBy readExpr spaces
+  char ')'
+  return $ List x
 
 readBoolean = do
-    bool <- char '#' >> oneOf "fFtT"
-    return . Boolean $ case toLower bool of
-                           't' -> True
-                           'f' -> False
+  bool <- char '#' >> oneOf "fFtT"
+  return . Boolean $ case toLower bool of
+               't' -> True
+               'f' -> False
 
 negNum = liftM ('-':) (char '-' >> many1 digit)
 
 readNumber = do
-    num <- try negNum <|> many1 digit
-    return . Number $ (read num :: Integer)
+  num <- try negNum <|> many1 digit
+  return . Number $ (read num :: Integer)
 
 readString = do
-    char '"'
-    str <- many1 (noneOf "\"")
-    char '"'
-    return $ String str
+  char '"'
+  str <- many1 (noneOf "\"")
+  char '"'
+  return $ String str
 
 parseSource :: String -> Either ParseError Expr 
 parseSource input = parse (skipMany space >> readExpr) "No Parse" input
 
 
-primitives :: [(Expr, [Expr] -> Either String Expr)]
+primitives :: [(Expr, [Expr] -> ErrorT EvalError IO Expr)]
 primitives = [(Symbol "+", arithmeticOp (+)),
-              (Symbol "-", arithmeticOp (-)),
-              (Symbol "*", arithmeticOp (*)),
-              (Symbol "/", arithmeticOp div),
-              (Symbol "<", compOp (<)),
-              (Symbol "<=", compOp (<=)),
-              (Symbol ">", compOp (>)),
-              (Symbol ">=", compOp (>=)),
-              (Symbol "=", compOp (==)),
-              (Symbol "and", logicOp and),
-              (Symbol "or", logicOp or),
-              (Symbol "not", notOp),
-              (Symbol "cons", cons),
-              (Symbol "list", list),
-              (Symbol "car", car),
-              (Symbol "cdr", cdr)]
+        (Symbol "-", arithmeticOp (-)),
+        (Symbol "*", arithmeticOp (*)),
+        (Symbol "/", arithmeticOp div),
+        (Symbol "<", compOp (<)),
+        (Symbol "<=", compOp (<=)),
+        (Symbol ">", compOp (>)),
+        (Symbol ">=", compOp (>=)),
+        (Symbol "=", compOp (==)),
+        (Symbol "and", logicOp and),
+        (Symbol "or", logicOp or),
+        (Symbol "not", notOp),
+        (Symbol "cons", cons),
+        (Symbol "list", list),
+        (Symbol "car", car),
+        (Symbol "cdr", cdr)]
 
 isNumberExpr :: Expr -> Bool
 isNumberExpr (Number _) = True
@@ -104,65 +122,58 @@ isListExpr :: Expr -> Bool
 isListExpr (List _) = True
 isListExpr _ = False
 
-arithmeticOp :: (Integer -> Integer -> Integer) -> [Expr] -> Either String Expr
+arithmeticOp :: (Integer -> Integer -> Integer) -> [Expr] -> ErrorT EvalError IO Expr
 arithmeticOp op exprs 
-    | all isNumberExpr exprs = Right . Number $ foldl1 op [x | (Number x) <- exprs]
-    | otherwise = Left "**Error: \
-          \Arithmetic expressions are of the form: ([+/-/*//] Num1 Num2 .. NumN)**"
+  | all isNumberExpr exprs = return . Number . foldl1 op $ [x | (Number x) <- exprs]
+  | otherwise = throwError $ SyntaxError "**Error: \
+      \Arithmetic expressions are of the form: ([+/-/*//] Num1 Num2 .. NumN)**"
 
-compOp :: (Integer -> Integer -> Bool) -> [Expr] -> Either String Expr
-compOp op [Number a, Number b] = Right . Boolean $ op a b
-compOp _ _ = Left "**Error: \
-    \Comparison expressions are of the form: ([</<=/>/>=/=] Num1 Num2)**"
+compOp :: (Integer -> Integer -> Bool) -> [Expr] -> ErrorT EvalError IO Expr
+compOp op [Number a, Number b] = return . Boolean $ op a b
+compOp _ _ = throwError $ SyntaxError "**Error: \
+  \Comparison expressions are of the form: ([</<=/>/>=/=] Num1 Num2)**"
 
-logicOp :: ([Bool] -> Bool) -> [Expr] -> Either String Expr
+logicOp :: ([Bool] -> Bool) -> [Expr] -> ErrorT EvalError IO Expr
 logicOp op exprs
-    | all isBoolExpr exprs = Right . Boolean . op $ [b | (Boolean b) <- exprs]
-    | otherwise = Left "**error: \
-          \Logical operators are of the form: ([and/or] expr1 expr2 .. exprN)**"
+  | all isBoolExpr exprs = return . Boolean . op $ [b | (Boolean b) <- exprs]
+  | otherwise = throwError $ SyntaxError "**Error: \
+      \Logical operators are of the form: ([and/or] expr1 expr2 .. exprN)**"
 
-notOp :: [Expr] -> Either String Expr
-notOp [Boolean b] = Right . Boolean . not $ b
-notOp _ = Left "**Error: \
-    \Not logical operators are of the form: (not expr1)**"
+notOp :: [Expr] -> ErrorT EvalError IO Expr
+notOp [Boolean b] = return . Boolean . not $ b
+notOp _ = throwError $ SyntaxError "**Error: \
+ \Not logical operators are of the form: (not expr1)**"
 
-car :: [Expr] -> Either String Expr
-car [List xs] = Right . head $ xs
-car _ = Left "**Error: car takes a list of data**"
+car :: [Expr] -> ErrorT EvalError IO Expr
+car [List xs] = return . head $ xs
+car _ = throwError $ SyntaxError "**Error: car takes a list of data**"
 
+cdr :: [Expr] -> ErrorT EvalError IO Expr
+cdr [List xs] = return . List . tail $ xs
+cdr _ = throwError $ SyntaxError "**Error: cdr takes a list of data**"
 
-cdr :: [Expr] -> Either String Expr
-cdr [List xs] = Right . List . tail $ xs
-cdr _ = Left "**Error: cdr takes a list of data**"
+cons :: [Expr] -> ErrorT EvalError IO Expr
+cons [x, List xs] = return . List $ (x:xs)
+cons _ = throwError $ SyntaxError "**Error: cons expressions are of the form: (cons x (list ..))**"
 
-cons :: [Expr] -> Either String Expr
-cons [x, List xs] = Right . List $ (x:xs)
-cons _ = Left "**Error: cons expressions are of the form: (cons x (list ..))**"
+list :: [Expr] -> ErrorT EvalError IO Expr
+list = return . List 
 
-list :: [Expr] -> Either String Expr
-list = Right . List 
+eval :: Env -> Expr -> ErrorT EvalError IO Expr
+eval env (List (Symbol "if":pred:tbr:fbr:[])) = do
+  res <- liftIO . runErrorT . eval env $ pred
+  case res of
+    Left err -> throwError err
+    Right (Boolean val)
+      | val -> eval env tbr
+      | otherwise -> eval env fbr
 
-ifConditional :: [Expr] -> Either String Expr
-ifConditional [Boolean pred, tbr, fbr] 
-  | pred = eval tbr
-  | otherwise = eval fbr
+eval env expr@(List (func:args)) = 
+  case lookup func primitives of
+    Just f -> mapM (eval env) args >>= apply f
+    Nothing -> throwError . BindError . show $ func
 
-eval :: Expr -> Either String Expr
-eval (List (Symbol "if":pred:args)) =
-    case eval pred of
-        Left _ -> Left "**Error: malformed if expression**"
-        Right boolVal -> ifConditional (boolVal:args)
-
-eval expr@(List (func:args)) = 
-    case lookup func primitives of
-        (Just f) 
-            | null . lefts $ evalArgs -> apply f $ rights evalArgs
-            | otherwise -> Left . head . lefts $ evalArgs
-        Nothing -> Left (printf "**Error: function \"%s\" is unbound**" 
-                         (show func) :: String)
-    where evalArgs = map eval args
-
-eval expr = Right expr
-                        
-apply :: ([Expr] -> Either String Expr) -> [Expr] -> Either String Expr
+eval env expr = return expr
+            
+apply :: ([Expr] -> ErrorT EvalError IO Expr) -> [Expr] -> ErrorT EvalError IO Expr
 apply func args = func args
