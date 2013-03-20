19c2d3dbaa16328760b234dc5860602fb9c426e3
Clarence Chuahuico <cchuahuico@gmail.com>
2013-03-19 19:31:26 -0700

    Added environment to main

--- Schell/Main.hs
+++ Schell/Main.hs
@@ -7,18 +7,19 @@ import System.IO
 import Schell
 
 main :: IO ()
-main = forever $ do
-    putStr "schell> " >> hFlush stdout
-    input <- getLine
-    env <- createEnv
-    if input == ":q" 
-      then
-          exitSuccess
-       else
-          case parseSource input of 
-            (Left err) -> putStrLn $ show err
-            (Right expr) -> do
-              res <- liftIO . runErrorT . eval env $ expr
-              case res of
-                (Left err) -> putStrLn . show $ err
-                (Right res) -> putStrLn . show $ res
+main = do
+  env <- createEnv
+  forever $ do
+  putStr "schell> " >> hFlush stdout
+  input <- getLine
+  if input == ":q" 
+    then
+      exitSuccess
+     else
+       case parseSource input of 
+         (Left err) -> putStrLn $ show err
+         (Right expr) -> do
+           res <- liftIO . runErrorT . eval env $ expr
+           case res of
+             (Left err) -> putStrLn . show $ err
+             (Right res) -> putStrLn . show $ res
