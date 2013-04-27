9bb82928cf70ddf67008853267f0e703bf412038
Clarence Chuahuico <cchuahuico@gmail.com>
2013-04-26 21:46:07 -0700

    Added some more test files

---
+++ Test/lambda.scm
@@ -0,0 +1,9 @@
+((lambda (x) (* x x)) 11)
+(define cube 
+  (lambda (x)
+    (* x x)))
+(lambda (z) (* z z))
+((lambda (a b c) (+ a b c)) 5 7 9)
+(define arith
+  (lambda (proc x y z)
+    (proc x y z)))
