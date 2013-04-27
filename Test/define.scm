1e2a8ab0b0dfaee4d08b6dc9321c2d4f2002f0b9
Clarence Chuahuico <cchuahuico@gmail.com>
2013-04-27 13:20:37 -0700

    Added some functions to test

--- Test/define.scm
+++ Test/define.scm
@@ -11,5 +11,17 @@
       1
       (+ (fib (- n 1)) (fib (- n 2)))))
 
-(define (mul3 x y z)
-  (* x y z))
+(define (mul3 x y z) (* x y z))
+(define compose (lambda (f g) (lambda (x) (f (g x)))))
+(define repeat (lambda (f) (compose f f)))
+(define abs (lambda (n) ((if (> n 0) + -) 0 n)))
+
+(define combine (lambda (f)
+  (lambda (x y)
+      (if (null? x) (quote ())
+          (f (list (car x) (car y))
+             ((combine f) (cdr x) (cdr y)))))))
+
+(define zip (combine cons))
+(define take (lambda (n seq) (if (<= n 0) (quote ()) (cons (car seq) (take (- n 1) (cdr seq))))))
+(define drop (lambda (n seq) (if (<= n 0) seq (drop (- n 1) (cdr seq)))))
