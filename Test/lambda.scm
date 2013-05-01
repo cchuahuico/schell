b0139b27022902940492efdd7a1fcd47231aa618
Clarence Chuahuico <cchuahuico@gmail.com>
2013-05-01 11:41:39 -0700

    Combined lambda tests with define.scm

--- Test/lambda.scm
+++
@@ -1,9 +0,0 @@
-((lambda (x) (* x x)) 11)
-(define cube 
-  (lambda (x)
-    (* x x)))
-(lambda (z) (* z z))
-((lambda (a b c) (+ a b c)) 5 7 9)
-(define arith
-  (lambda (proc x y z)
-    (proc x y z)))
