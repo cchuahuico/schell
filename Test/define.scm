0845c4c07bfcce7e5225c803b30ba94aa9ff6f62
Clarence Chuahuico <cchuahuico@gmail.com>
2013-05-01 12:05:32 -0700

    Made test for define more extensive

--- Test/define.scm
+++ Test/define.scm
@@ -4,7 +4,7 @@
 (define (factorial n)
   (if (= n 0)
       1
-      (factorial (- n 1))))
+      (* n (factorial (- n 1)))))
 
 (define (fib n)
   (if (<= n 1)
@@ -25,3 +25,32 @@
 (define zip (combine cons))
 (define take (lambda (n seq) (if (<= n 0) (quote ()) (cons (car seq) (take (- n 1) (cdr seq))))))
 (define drop (lambda (n seq) (if (<= n 0) seq (drop (- n 1) (cdr seq)))))
+(define (map f seq) (if (null? seq) (quote ()) (cons (f (car seq)) (map f (cdr seq)))))
+
+(define Y
+ (lambda (X)
+  ((lambda (procedure)
+     (X (lambda (arg) ((procedure procedure) arg))))
+   (lambda (procedure)
+     (X (lambda (arg) ((procedure procedure) arg)))))))
+
+(define F*
+ (lambda (func-arg)
+  (lambda (n)
+    (if (= n 0)
+        1
+        (* n (func-arg (- n 1)))))))
+
+(define fact (Y F*))
+
+var
+(square 10)
+(factorial 20)
+(fib 11)
+(mul3 5 9 2)
+((compose list square) 3)
+((repeat square) 5)
+(list (abs -3) (abs 0) (abs 3))
+(take 2 (map square (list 11 7 0 3 9)))
+(drop 5 (list 1 2 3 4 5 6 7 8 9 10))
+(fact 8)
